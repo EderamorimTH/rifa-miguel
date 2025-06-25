347264c395d5172c3ebddffa34379efdac01a78a
EderamorimTH <eder.thiago123@gmail.com>
2025-06-25 09:16:44 -0400

    Update index.js

--- mp-api/index.js
+++ mp-api/index.js
@@ -146,7 +146,7 @@ app.post('/create_preference', async (req, res) => {
         items: [{
           title: `Rifa - ${quantity} número(s)`,
           quantity: Number(quantity),
-          unit_price: 10,
+          unit_price: 1,
         }],
         payer: {
           name: buyerName,
